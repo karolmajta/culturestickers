83c5e51dedcd544d88c0888f1986edb5e6accad5
Karol Majta <karolmajta@gmail.com>
2015-10-17 15:41:10 +0200

    Added spinner for images

--- src/js/ImagePicker.jsx
+++ src/js/ImagePicker.jsx
@@ -9,16 +9,43 @@ var images = [
 
 
 var ImagePicker = React.createClass({
+    componentDidMount: function () {
+        this.search();
+    },
+    getInitialState: function () {
+        return {
+            search: 'Picasso',
+            images: [],
+            loading: false
+        }
+    },
     render: function() {
         return (
             <div>
-                {_.map(images, function (i) {
-                    return (
-                        <img src={i.url} key={i.url} onClick={(function () { this.props.onImageSelect(i); }).bind(this)}  />
-                    );
-                }, this)}
+                <div className='form-group'>
+                    <input className='form-control search'
+                           value={this.state.search}
+                           onChange={(function (e) { this.setState({search: e.target.value}); }).bind(this)}
+                           onKeyUp={(function (e) { if (e.key == 'Enter') { this.search() } }).bind(this)} />
+                </div>
+                <div>
+                    {_.map(this.state.images, function (i) {
+                        return (
+                            <img src={i.url} key={i.url} onClick={(function () { this.props.onImageSelect(i); }).bind(this)}  />
+                        );
+                    }, this)}
+                    <div>
+                        {this.state.loading ? <i className="fa fa-spinner fa-4x fa-spin"></i> : undefined}
+                    </div>
+                </div>
             </div>
         );
+    },
+    search: function () {
+        this.setState({loading: true, images: []});
+        window.setTimeout((function () {
+            this.setState({loading: false, images: images});
+        }).bind(this), 2000);
     }
 });
 
